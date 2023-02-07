368b183df65eaf9e74fe48c3496428440ccd9ce3
devjorgesoto <devjorgesoto@gmail.com>
2023-02-07 14:27:23 -0900

    updates

--- final_template/onlinecourse/admin.py
+++ final_template/onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner, Question, Choice
+from .models import Instructor, Learner, Course, Lesson, Exam, Question, Choice
 
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
@@ -19,23 +19,45 @@ class LessonInline(admin.StackedInline):
 
 
 # Register your models here.
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_learners', 'full_time']
+    list_filter =  ['full_time']
+
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ['user','occupation', 'social_link']
+    list_filter = ['occupation']
+
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
+    list_display = ('name','pub_date', 'total_enrollment')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
-
-
+    
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class ExamAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline, ChoiceInline]
+    list_display = ['exam_title']
+ 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines =[ChoiceInline]
+    list_display = ['question_content', 'choice']
 
-# <HINT> Register Question and Choice models here
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_content']
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# Admin Site Register
+
+admin.site.register(Instructor,InstructorAdmin)
+admin.site.register(Learner,LearnerAdmin)
 
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
+#admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Exam, ExamAdmin)
+
+#admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice, ChoiceAdmin)
--- final_template/onlinecourse/models.py
+++ final_template/onlinecourse/models.py
@@ -5,26 +5,24 @@ try:
 except Exception:
     print("There was an error loading django modules. Do you have django installed?")
     sys.exit()
-
 from django.conf import settings
 import uuid
 
-
-# Instructor model
 class Instructor(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+
     full_time = models.BooleanField(default=True)
     total_learners = models.IntegerField()
 
     def __str__(self):
         return self.user.username
 
-
-# Learner model
 class Learner(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -33,11 +31,13 @@ class Learner(models.Model):
     DEVELOPER = 'developer'
     DATA_SCIENTIST = 'data_scientist'
     DATABASE_ADMIN = 'dba'
+    AI_SCIENTIST = 'ai_scientist'
     OCCUPATION_CHOICES = [
         (STUDENT, 'Student'),
         (DEVELOPER, 'Developer'),
         (DATA_SCIENTIST, 'Data Scientist'),
-        (DATABASE_ADMIN, 'Database Admin')
+        (DATABASE_ADMIN, 'Database Admin'),
+        (AI_SCIENTIST, 'AI Scientist')
     ]
     occupation = models.CharField(
         null=False,
@@ -49,37 +49,100 @@ class Learner(models.Model):
 
     def __str__(self):
         return self.user.username + "," + \
-               self.occupation
+            self.occupation
 
+class Exam (models.Model):
+    id = models.BigAutoField(primary_key=True)
 
-# Course model
+    exam_title = models.CharField(max_length=100)
+
+class Choice(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+        #  <HINT> Create a Choice Model with:
+        # Summary: Used to persist choice content for a question
+        # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
+        # Choice content
+        # Indicate if this choice of the question is a correct one or not
+        # Other fields and methods you would like to design
+
+    exam = models.ForeignKey (Exam , on_delete=models.CASCADE)
+
+    choice_content = models.CharField(max_length=100)
+    #choice_boolean = models.BooleanField(default=False)
+
+class Question(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    question_content = models.CharField(max_length=100)
+
+    exam = models.ForeignKey (Exam , on_delete=models.CASCADE)
+    choice = models.ForeignKey (Choice, on_delete=models.CASCADE)
+
+    #question_grade = models.IntegerField(default=0)
+    #question_grade_point = models.IntegerField(default=0)
+
+    # <HINT> Create a Question Model with:
+    # Summary: Used to persist question content for a course
+    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
+    # Foreign key to lesson
+    # Has a grade point for each question / # question grade/mark / # question_grade_point
+    # Has question content / #question content
+    # question text
+    # Other fields and methods you would like to design
+
+    # REVIEW THIS IF NEEDED !!!
+    # <HINT> A sample model method to calculate if learner get the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(
+            is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
 class Course(models.Model):
-    name = models.CharField(null=False, max_length=30, default='online course')
+    id = models.BigAutoField(primary_key=True)
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
+    instructors = models.ManyToManyField(Instructor)
+    #lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE) #lesson = models.ManyToOneRel(Lesson)
+
+
+    name = models.CharField(null=False, max_length=100, default='online course')
     image = models.ImageField(upload_to='course_images/')
     description = models.CharField(max_length=1000)
     pub_date = models.DateField(null=True)
-    instructors = models.ManyToManyField(Instructor)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
+    
     total_enrollment = models.IntegerField(default=0)
     is_enrolled = False
 
+    # def __str__(self):
+        #    return "Name: " + self.name + "," + \
+        #           "Description: " + self.description
+
     def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
+        return self.name
 
-
-# Lesson model
 class Lesson(models.Model):
-    title = models.CharField(max_length=200, default="title")
-    order = models.IntegerField(default=0)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    content = models.TextField()
+    id = models.BigAutoField(primary_key=True)
 
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE) #exam = models.ManyToOneRel(Exam)
+    course = models.ForeignKey (Course , on_delete=models.CASCADE)
 
-# Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
-# And we could use the enrollment to track information such as exam submissions
+    title = models.CharField(max_length=200, default="Course Title") 
+    description = models.CharField( max_length=200, default="Course Description")
+    lecture = models.TextField(max_length=1000, default="Course Lecture")
+
 class Enrollment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+        # Enrollment model
+        # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
+        # And we could use the enrollment to track information such as exam submissions
+
+
     AUDIT = 'audit'
     HONOR = 'honor'
     BETA = 'BETA'
@@ -90,61 +153,19 @@ class Enrollment(models.Model):
     ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
-
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
-class Question(models.Model):
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
-
-    # Foreign key to lesson
-    user = models.ForeignKey(Course, on_delete=models.CASCADE)
-    
-    #question content
-    question_content = models.CharField(max_length=5)
-    # question text
-    question_text= models.CharField(max_length=5)
-    
-    # question grade/mark
-    question_grade= models.IntegerField(default=0)
-    # question_grade_point
-    question_grade_point= models.IntegerField(default=0)
-
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    def is_get_score(self, selected_ids):
-        all_answers = self.choice_set.filter(is_correct=True).count()
-        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
-            return True
-        else:
-            return False
-
-
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
-class Choice(models.Model):
-    choice_content = models.CharField(max_length=200)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
-
-    choice_result = models.BooleanField(default= False)
-    choice_options = models.CharField(max_length=100)
-
-# <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
 class Submission(models.Model):
-    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    choices = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    
+    id = models.BigAutoField(primary_key=True)
+    # <HINT> The submission model
+    # One enrollment could have multiple submission
+    # One submission could have multiple choices
+    # One choice could belong to multiple submissions
+
+    enrollment = models.ManyToManyField(Enrollment)
+    choice = models.ManyToManyField(Choice)
+
